facd3bac1552660874712ac16c2c9fd8a5d9b247
Reuven Lerner <reuven@lerner.co.il>
2024-01-04 07:12:47 +0200

    gitautosync update 95

--- ipython_log.py
+++ ipython_log.py
@@ -452,3 +452,18 @@ def calc(n1=First, op=Oper, n2=Second):
   else:
     result = f'Either of inputs are non-numeric'
   return(f'{n1} {op} {n2} = {result}')
+First = int(input('Enter the First Number :').strip())
+Second = int(input('Enter the Second Number :').strip())
+Oper = input('Enter the operator +, -').strip()
+
+def calc(n1, op, n2):
+  if n1.isdigit() and n2.isdigit():
+    if op == '+':
+      result = n1 + n2
+    elif op == '-':
+      result = n1-n2
+    else:
+      result = f'Operator is undefined'
+  else:
+    result = f'Either of inputs are non-numeric'
+  return(f'{n1} {op} {n2} = {result}')
