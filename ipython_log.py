cf9ef2fac6d8b04557f15fd922eeed612af840dd
Reuven Lerner <reuven@lerner.co.il>
2024-01-04 05:41:22 +0200

    gitautosync update 5

--- ipython_log.py
+++ ipython_log.py
@@ -9,3 +9,5 @@ z = {'a':10, 'b':20, 'c':30}
 
 # how can I print x, y, and z -- both the variable names and their values?
 print(f'x={x}, y={y}, z={z}')
+# in modern versions of Python, you can use special syntax as a shortcut to the above:
+print(f'{x=}, {y=}, {z=}')
