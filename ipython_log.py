bdd238ba93187aeb69b1d93d81acc2c8d28d8f48
Reuven Lerner <reuven@lerner.co.il>
2024-01-04 08:43:33 +0200

    gitautosync update 170

--- ipython_log.py
+++ ipython_log.py
@@ -897,3 +897,16 @@ help(str.lower)   # get help on the lower method for strings
 help(input(
 help(input)
 help(print)
+def frequencies(s):
+    output = {}
+
+    for one_character in s:
+        if one_character in output:     # have we seen this character before?
+            output[one_character] += 1  #   if so, add 1 to its count
+        else:                           # no, it's the first time?
+            output[one_character] = 1   #   that's OK -- add the key-value pair to the dict
+            
+
+    return output
+
+frequencies('hello out there')
