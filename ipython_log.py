fb8f0f300b7d74987c62ce5ef0a0d4531b7d517a
Reuven Lerner <reuven@lerner.co.il>
2024-01-04 05:40:50 +0200

    gitautosync update 4

--- ipython_log.py
+++ ipython_log.py
@@ -1,3 +1,11 @@
 # IPython log file
 
 
+# f-strings and printing values
+
+x = 10
+y = [10, 20, 30]
+z = {'a':10, 'b':20, 'c':30}
+
+# how can I print x, y, and z -- both the variable names and their values?
+print(f'x={x}, y={y}, z={z}')
