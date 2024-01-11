196a4e89038d27077c2bec84e4cd89404bf8c8ff
Reuven Lerner <reuven@lerner.co.il>
2024-01-11 05:46:23 +0200

    gitautosync update 4

--- ipython_log.py
+++ ipython_log.py
@@ -1,3 +1,17 @@
 # IPython log file
 
 
+def count_ips(filename):
+    output = {}
+
+    for one_line in open(filename):
+        ip_address = one_line.split()[0]
+
+        if ip_address in output:
+            output[ip_address] += 1  # seen before? add 1
+        else:
+            output[ip_address] = 1   # first time? set to 1
+
+    return output
+
+print(count_ips('logfile.txt'))
