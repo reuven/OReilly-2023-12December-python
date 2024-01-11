ba9c046297c4fa6b40fa4d0ccf41213f520656aa
Reuven Lerner <reuven@lerner.co.il>
2024-01-11 08:36:40 +0200

    gitautosync update 120

--- ipython_log.py
+++ ipython_log.py
@@ -272,3 +272,26 @@ while True:
         rich.print('[blue]Too high[/blue]')
         
         
+import random
+import rich
+
+number = random.randint(0, 100)  
+
+while True:
+    s = input('Guess the number: ').strip()
+
+    if not s.isdigit():
+        print(f'{s} is not numeric; try again')
+        continue
+
+    guess = int(s)    # get an integer based on the user's input
+
+    if guess == number:
+        rich.print('[yellow on red]You got it[/yellow on red]!')
+        break
+    elif guess < number:
+        rich.print('[red]Too low[/red]')
+    else:
+        rich.print('[lightblue]Too high[/lightblue]')
+        
+        
