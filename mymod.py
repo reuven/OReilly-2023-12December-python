3a7a5ecc2fd312cc6f004fe1cc268d7d93b7b60d
Reuven Lerner <reuven@lerner.co.il>
2024-01-11 07:29:02 +0200

    gitautosync update 83

--- mymod.py
+++ mymod.py
@@ -10,5 +10,5 @@ z = {'a':100, 'b':200, 'c':300}
 def hello(name):
     return f'Hello, {name}!'
 
-if __name__ == '__main__':
+if __name__ == '__main__':   # this means: never print when mymod.py is imported, only when executed
     print(f'Goodbye from {__name__}!')
